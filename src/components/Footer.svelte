54894218e7223a9519262a85c6e6ad68b83208ad
me7 <gampolt@gmail.com>
2019-12-14 05:15:29 +0700

    Nav and Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,18 @@
-Footer
+<div >
+  <a href="https://github.com/me7/realworld_svelte_nim">Fork on Github</a>
+</div>
+
+<style>
+div {
+  background: linear-gradient(#556, #333);
+  position: fixed;
+  z-index: 999;
+  text-align: center;
+  padding: 1rem 2rem;
+  width: 100vw;
+}
+
+a{
+  color: white;
+}
+</style>
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,2 +1,26 @@
-<a href="/">Home</a>
-<a href="#/setting">Setting</a>
+<nav class="flex justify-between items-center">
+  <div class="text-lg">
+    <a class="brand" href="/">Conduit</a>
+  </div>
+  <div>
+    <a href="/">Home</a>
+    <a href="#/login">Sign in</a>
+    <a href="#/register">Sign up</a>
+  </div>
+</nav>
+
+<style>
+a{
+  text-decoration: none;
+  padding: 0rem .5rem;
+  color: #888;
+}
+
+a:hover{
+  color: #000
+}
+
+.brand{
+  color: #6c6
+}
+</style>
