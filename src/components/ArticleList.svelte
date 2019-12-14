32407066f4ab33ad28b8170cbe945a995f7bb13d
me7 <gampolt@gmail.com>
2019-12-15 03:31:09 +0700

    ArticleList.svelte

---
+++ src/components/ArticleList.svelte
@@ -0,0 +1,25 @@
+<p>ArticleList</p>
+<div class="border-solid border-gray-600">
+  {#if articles}
+    <pre>{articlesCount}</pre>
+    <pre>{articles}</pre>
+  {:else}
+    Loading...
+  {/if}
+
+</div>
+
+
+<script>
+  import * as api from 'api.js'
+
+  let articles
+  let articlesCount
+
+  async function getData(){
+    articles = null
+    ({articles, articlesCount} = await api.get('/articles'))
+  }
+
+  $: getData()
+</script>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,18 +1,6 @@
-<button on:click={getArticles}>Get Data</button>
-{#each posts as p}
-<h1>{p}</h1>
-{/each}
-
+<ArticleList />
 
 <script>
   import * as api from 'api.js'
-  let posts = []
-
-  async function getArticles(){
-    let data = await api.get('/articles')
-    console.log(data)
-    for(let i=0;i<20;i++){
-      posts = [...posts, data.articles[i].title]
-    }
-  }
+  import ArticleList from '../components/ArticleList.svelte'
 </script>
