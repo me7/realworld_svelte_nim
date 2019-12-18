96a95202b6f75c46b6e1814ae1b382d1929dfe76
me7 <gampolt@gmail.com>
2019-12-18 17:55:48 +0700

    login textbox width

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,10 +1,15 @@
 <div class="flex flex-col items-center">
   <h2 class="text-4xl font-semibold">Sign in</h2>
-  <a href="#/register">Need an account?</a>
+  <a class="text-green-500" href="#/register">Need an account?</a>
 
-  <form class="flex flex-col mt-3" action="#/">
-    <input class="p-3" type="text" placeholder="Email" />
-    <input class="p-3 mt-1" type="text" placeholder="Password" />
-    <button class="ml-auto px-6 py-3 rounded bg-green-600 text-white text-xl" type="submit">Sign in</button>
-  </form>
 </div>
+
+<form class="flex flex-col mt-3 px-4" action="#/">
+  <input class="p-3" type="text" placeholder="Email" />
+  <input class="p-3 mt-1" type="text" placeholder="Password" />
+  <button
+    class="ml-auto px-6 py-3 rounded bg-green-600 text-white text-xl"
+    type="submit">
+    Sign in
+  </button>
+</form>
